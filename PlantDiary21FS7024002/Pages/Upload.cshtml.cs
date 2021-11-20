439e11d2c431ae68b0945f0e636151c87ba321bf
jonesbr <brandan.jones@uc.edu>
2021-11-20 12:51:23 -0500

    Handle XML validation and exceptions

--- PlantDiary21FS7024002/Pages/Upload.cshtml.cs
+++ PlantDiary21FS7024002/Pages/Upload.cshtml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Schema;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,7 @@ namespace PlantDiary21FS7024002.Pages
 
         public void OnGet()
         {
+            ViewData["Result"] = "Not processed";
         }
 
         public void OnPost()
@@ -42,6 +44,49 @@ namespace PlantDiary21FS7024002.Pages
             XmlNode node = doc.SelectSingleNode("/plant/genus");
             int foo = 1 + 1;
 
+            ValidateXml(file);
+
+        }
+
+        private void ValidateXml(string file)
+        {
+            // declare our validation preference
+            XmlReaderSettings settings = new XmlReaderSettings();
+            string xsdPath = Path.Combine(Environment.ContentRootPath, "uploads", "plants.xsd");
+            settings.Schemas.Add(null, xsdPath);
+
+            // we want to validate with XSD
+            settings.ValidationType = ValidationType.Schema;
+
+            // a couple more flags
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ProcessSchemaLocation;
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ReportValidationWarnings;
+
+            settings.ValidationEventHandler += new System.Xml.Schema.ValidationEventHandler(this.ValidationEventHandler);
+
+            XmlReader xmlReader = XmlReader.Create(file, settings);
+            try
+            {
+                while (xmlReader.Read())
+                {
+
+                }
+                ViewData["Result"] = "Validation Passed.";
+            } 
+            catch (Exception e)
+            {
+                ViewData["Result"] = "Validation Failed.  " + e.Message;
+            }
+        }
+
+        /// <summary>
+        /// Only if a validation occurs, will this method be invoked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void ValidationEventHandler(object sender, ValidationEventArgs args)
+        {
+            throw new Exception("validation failed.  Message: " + args.Message);
         }
     }
 }
